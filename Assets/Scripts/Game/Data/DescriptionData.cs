e554673d67b2646e1a853751c6cb4477a069a4b1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-09-19 05:23:12 +0900

    説明UIのデータを作成中

---
+++ Assets/Scripts/Game/Data/DescriptionData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	[System.Serializable][CreateAssetMenu()]
+	public class DescriptionData : ScriptableObject
+	{
+		[System.Serializable]
+		public class Element
+		{
+			[SerializeField]
+			private StringAsset.Finder title;
+
+			[SerializeField]
+			private StringAsset.Finder message;
+
+			[SerializeField]
+			private Image image;
+		}
+
+		[SerializeField]
+		private List<Element> database;
+	}
+}
---
+++ Assets/Scripts/Game/DescriptionManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class DescriptionManager : SingletonMonoBehaviour<DescriptionManager>
+	{
+		public void Deploy(CharacterData data)
+		{
+			
+		}
+
+		public void Deploy(DescriptionData data)
+		{
+			
+		}
+	}
+}
