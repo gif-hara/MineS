b9fd6d92e00e77807f1cf948c78b6cdc608a2043
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-09-19 17:46:29 +0900

    MPを表示するよう実装

---
+++ Assets/Scripts/Game/UI/OnModifiedCharacterDataSetGaugeMagicPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class OnModifiedCharacterDataSetGaugeMagicPoint : MonoBehaviour, IReceiveModifiedCharacterData
+	{
+		[SerializeField]
+		private Gauge target;
+
+#region IReceiveModifiedCharacterData implementation
+
+		public void OnModifiedCharacterData(CharacterData data)
+		{
+			this.target.Set((float)data.MagicPoint / data.MagicPointMax);
+		}
+
+#endregion
+	}
+}
