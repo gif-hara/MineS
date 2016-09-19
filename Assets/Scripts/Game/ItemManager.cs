0eea9bc6e2e7917197291047e79e2f4ce13ffbd8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-09-19 23:27:18 +0900

    アイテムのマスターデータ周りのリファクタリング

--- Assets/Scripts/Game/ItemManager.cs
+++ Assets/Scripts/Game/ItemManager.cs
@@ -12,47 +12,7 @@ namespace MineS
 	public class ItemManager : SingletonMonoBehaviour<ItemManager>
 	{
 		[SerializeField]
-		private UsableItemMasterData usableItemMasterData;
-
-		[SerializeField]
-		private WeaponMasterData weaponMasterData;
-
-		[SerializeField]
-		private ShieldMasterData shieldMasterData;
-
-		[SerializeField]
-		private HelmetMasterData helmetMasterData;
-
-		[SerializeField]
-		private BodyMasterData bodyMasterData;
-
-		[SerializeField]
-		private GloveMasterData gloveMasterData;
-
-		[SerializeField]
-		private LegMasterData legMasterData;
-
-		[SerializeField]
-		private AccessoryMasterData accessoryMasterData;
-
-		[SerializeField]
 		private List<InventoryObserver> observers;
-
-		public UsableItemMasterData UsableItemMasterData{ get { return this.usableItemMasterData; } }
-
-		public WeaponMasterData WeaponMasterData{ get { return this.weaponMasterData; } }
-
-		public ShieldMasterData ShieldMasterData{ get { return this.shieldMasterData; } }
-
-		public HelmetMasterData HelmetMasterData{ get { return this.helmetMasterData; } }
-
-		public BodyMasterData BodyMasterData{ get { return this.bodyMasterData; } }
-
-		public GloveMasterData GloveMasterData{ get { return this.gloveMasterData; } }
-
-		public LegMasterData LegMasterData{ get { return this.legMasterData; } }
-
-		public AccessoryMasterData AccessoryMasterData{ get { return this.accessoryMasterData; } }
 
 		public void OpenInventory()
 		{
