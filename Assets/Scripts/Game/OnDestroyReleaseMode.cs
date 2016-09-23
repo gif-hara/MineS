1339f891ab9913f6ef4290b037298898ef8c1671
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-09-24 03:10:54 +0900

    デバッグパネルを実装

---
+++ Assets/Scripts/Game/OnDestroyReleaseMode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MineS
+{
+    public class OnDestroyReleaseMode : MonoBehaviour
+    {
+#if Release
+        void Awake()
+        {
+            Destroy(this.gameObject);
+        }
+#endif
+    }
+}
