e16020586d970014ccd6a9f71f7dce2b3ef1878b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-10-03 22:31:35 +0900

    SystemManagerを追加

---
+++ Assets/Scripts/Game/SystemManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class SystemManager : MonoBehaviour
+	{
+		void Awake()
+		{
+			QualitySettings.vSyncCount = 1;
+			Application.targetFrameRate = 60;
+		}
+	}
+}
