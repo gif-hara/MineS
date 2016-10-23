374a0ed4cda70ac33d9bef50f5218aad5496fd6e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-10-23 22:17:48 +0900

    BGM入れ込み

---
+++ Assets/Scripts/Game/BGMManager.cs
@@ -0,0 +1,106 @@
+﻿#define BGMDATA_EDITMODE
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class BGMManager : SingletonMonoBehaviour<BGMManager>
+	{
+		[SerializeField]
+		private AudioSource source;
+
+		[SerializeField]
+		private AudioClip debugClip;
+
+		[SerializeField]
+		private float startTime;
+
+		[SerializeField]
+		private List<BGMData> database;
+
+		private BGMData currentData = null;
+
+#if BGMDATA_EDITMODE
+		void Start()
+		{
+			if(this.debugClip != null)
+			{
+				this.StartBGM(this.debugClip);
+				this.source.time = this.startTime;
+			}
+		}
+#endif
+
+		void Update()
+		{
+			if(!this.source.isPlaying)
+			{
+				return;
+			}
+
+			if(this.source.time >= this.currentData.StartLoop)
+			{
+				this.source.time = this.currentData.EndLoop;
+#if BGMDATA_EDITMODE
+				Debug.Log("Loop");
+#endif
+			}
+
+#if BGMDATA_EDITMODE
+			if(!Input.GetKey(KeyCode.LeftShift))
+			{
+				if(Input.GetKeyDown(KeyCode.LeftArrow))
+				{
+					this.currentData.startLoop -= 0.001f;
+					Debug.LogFormat("startLoop = {0}", this.currentData.startLoop);
+					this.SetStartTime();
+				}
+				if(Input.GetKeyDown(KeyCode.RightArrow))
+				{
+					this.currentData.startLoop += 0.001f;
+					Debug.LogFormat("startLoop = {0}", this.currentData.startLoop);
+					this.SetStartTime();
+				}
+			}
+			else
+			{
+				if(Input.GetKeyDown(KeyCode.LeftArrow))
+				{
+					this.currentData.endLoop -= 0.001f;
+					Debug.LogFormat("endLoop = {0}", this.currentData.endLoop);
+					this.SetStartTime();
+				}
+				if(Input.GetKeyDown(KeyCode.RightArrow))
+				{
+					this.currentData.endLoop += 0.001f;
+					Debug.LogFormat("endLoop = {0}", this.currentData.endLoop);
+					this.SetStartTime();
+				}
+			}
+
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				Debug.LogFormat("time = {0}", this.source.time);
+			}
+#endif
+		}
+
+		public void StartBGM(AudioClip clip)
+		{
+			this.currentData = this.database.Find(d => d.Clip == clip);
+			this.source.clip = clip;
+			this.source.Play();
+		}
+
+		[ContextMenu("Set StartTime")]
+		private void SetStartTime()
+		{
+			this.source.time = this.startTime;
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Data/BGMData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	[CreateAssetMenu()]
+	public class BGMData : ScriptableObject
+	{
+		[SerializeField]
+		public AudioClip clip;
+
+		[SerializeField]
+		public float startLoop;
+
+		[SerializeField]
+		public float endLoop;
+
+		public AudioClip Clip{ get { return this.clip; } }
+
+		public float StartLoop{ get { return this.startLoop; } }
+
+		public float EndLoop{ get { return this.endLoop; } }
+	}
+}
