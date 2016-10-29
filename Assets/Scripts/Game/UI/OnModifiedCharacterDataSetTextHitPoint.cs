6d99b05a710ac8aa0427bfd895618739fc9f4cb3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-10-29 12:48:08 +0900

    ヒットポイントが最大値を超えた場合UIの色を変更するよう実装

--- Assets/Scripts/Game/UI/OnModifiedCharacterDataSetTextHitPoint.cs
+++ Assets/Scripts/Game/UI/OnModifiedCharacterDataSetTextHitPoint.cs
@@ -17,11 +17,18 @@ namespace MineS
 		[SerializeField]
 		private StringAsset.Finder format;
 
+		[SerializeField]
+		private Color basicColor;
+
+		[SerializeField]
+		private Color limitOverColor;
+
 #region IReceiveModifiedCharacterData implementation
 
 		public void OnModifiedCharacterData(CharacterData data)
 		{
 			this.target.text = this.format.Format(data.HitPoint, data.HitPointMax);
+			this.target.color = data.HitPoint > data.HitPointMax ? this.limitOverColor : this.basicColor;
 		}
 
 #endregion
