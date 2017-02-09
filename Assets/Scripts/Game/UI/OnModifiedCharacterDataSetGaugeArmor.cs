2b29689453ebf1a250a5e5578d5a9279c6a9c2ac
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-02-10 01:56:22 +0900

    ヒットポイントとアーマーのゲージを実装

---
+++ Assets/Scripts/Game/UI/OnModifiedCharacterDataSetGaugeArmor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class OnModifiedCharacterDataSetGaugeArmor : MonoBehaviour, IReceiveModifiedCharacterData
+	{
+		[SerializeField]
+		private Gauge target;
+
+#region IReceiveModifiedCharacterData implementation
+
+		public void OnModifiedCharacterData(CharacterData data)
+		{
+		    var value = data.ArmorMax <= 0 ? 0.0f : (float) data.Armor / data.ArmorMax;
+		    value = value > 1.0f ? 1.0f : value;
+		    this.target.Set(value);
+		}
+
+#endregion
+	}
+}
---
+++ Assets/Scripts/Game/UI/OnModifiedCharacterDataSetGaugeHitPoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace MineS
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class OnModifiedCharacterDataSetGaugeHitPoint : MonoBehaviour, IReceiveModifiedCharacterData
+	{
+		[SerializeField]
+		private Gauge target;
+
+#region IReceiveModifiedCharacterData implementation
+
+		public void OnModifiedCharacterData(CharacterData data)
+		{
+		    var value = (float) data.HitPoint / data.HitPointMax;
+		    value = value > 1.0f ? 1.0f : value;
+		    this.target.Set(value);
+		}
+
+#endregion
+	}
+}
